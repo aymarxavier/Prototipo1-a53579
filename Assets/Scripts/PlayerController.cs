7b54fe1a88d590cf5568f40dfe858911a293b10f
aymarxavier <a53579@alunos.ipb.pt>
2024-04-23 15:24:20 +0100

    Entre no banco do motorista, 1.4

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    private float speed = 15.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float verticalInput;
+    private float forwardInput;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,9 +18,17 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
+    
+
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+
         //Mover o veiculo para frente
+
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        //viramos o veiculo
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
